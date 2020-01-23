4439efb694a0c4fce131f4acec71351f5ad0beb9
Mia <tamiapaschall@gmail.com>
2020-01-23 06:20:20 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
 class Waiter
-
+attr_accessor :name, :assert_generates
+  
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
 end
